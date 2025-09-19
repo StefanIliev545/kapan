f4b4acbe21f7c5f9525863abf76850e31113c5bd
StefanIliev545 <stefan@obscu.ro>
2025-09-19 18:32:13 +0300

    Dumping.

--- packages/nextjs/.storybook/main.ts
+++ packages/nextjs/.storybook/main.ts
@@ -1,18 +1,36 @@
+import { createRequire } from "node:module";
+import { dirname, join } from "node:path";
 import type { StorybookConfig } from "@storybook/nextjs";
+
+const require = createRequire(import.meta.url);
 
 const config: StorybookConfig = {
   stories: ["../**/*.stories.@(ts|tsx|mdx)"],
-  addons: ["msw-storybook-addon"],
+  addons: [getAbsolutePath("msw-storybook-addon")],
+
   framework: {
-    name: "@storybook/nextjs",
+    name: getAbsolutePath("@storybook/nextjs"),
     options: {
       nextConfigPath: "../next.config.js",
+      
     },
   },
-  staticDirs: ["../public"],
-  docs: {
-    autodocs: "tag",
+  core: {
+    builder: {
+      name: '@storybook/builder-webpack5',
+      options: {
+        fsCache: true,         // enables Webpack’s filesystem cache between runs
+        lazyCompilation: true, // faster startup; compiles on demand in dev
+      },
+    },
   },
+
+
+  staticDirs: ["../public"]
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
